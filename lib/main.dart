53399db61cd5701a730dfaca662f4f4276eb6389
SimonL14 <112928575+SimonL14@users.noreply.github.com>
2022-11-23 14:44:18 +0100

    sqflite and path package setup

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,8 @@ import 'package:flutter/material.dart';
 //import 'package:flutter_riverpod/flutter_riverpod.dart';
 //import 'package:go_router/go_router.dart';
 //import 'package:json_annotation/json_annotation.dart';
+//import 'package:sqflite/sqflite.dart';
+//import 'package:path/path.dart';
 
 
 void main() {
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,7 +6,9 @@ import FlutterMacOS
 import Foundation
 
 import location
+import sqflite
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
+  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }

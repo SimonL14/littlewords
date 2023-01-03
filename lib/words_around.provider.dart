5f5985b6f5035659b18c787aa9e709db4cbc3219
SimonL14 <simon-bcsm-11@hotmail.fr>
2023-01-03 12:53:41 +0100

    Update WordsAroundProvider

--- lib/words_around.provider.dart
+++ lib/words_around.provider.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:convert';
 
 import 'package:dio/dio.dart';
@@ -8,30 +9,40 @@ import 'package:littlewords/dio.provider.dart';
 import 'package:littlewords/word_dto.dart';
 import 'package:littlewords/words_dto.dart';
 
-final wordsAroundProvider = FutureProvider<List<WordDTO>>((ref) async{
-  AsyncValue<LatLng?> location = ref.watch(deviceLocationProvider);
-
-  return location.map(data: (data) async {
-    print('try to get words around ${data.value!.latitude} ${data.value!.longitude}');
-    
-    final Dio dio = ref.read(dioProvider);
-    Response response = await dio.get(
-        '/word/around?latitude=${data.value!.latitude}&longitude=${data.value!.longitude}');
-
-    var jsonAsString = response.toString();
-    var json = jsonDecode(jsonAsString);
-
-    final WordsDTO wordsDTO = WordsDTO.fromJson(json);
-    if (wordsDTO.data == null) {
-      return Future.value([]);
-    }
-
-    return Future.value(wordsDTO.data!);
-  }, error: (error) {
-    print(error);
-    return Future.value([]);
-  }, loading: (loading) {
-    print('loading');
-    return Future.value([]);
-  });
+final wordsAroundProvider = FutureProvider<List<WordDTO>>((ref) async {
+  return ref
+      .watch(deviceLocationProvider)
+      .when(data: (data) => _onData(data, ref), error: _onError, loading: _onLoading);
 });
+
+FutureOr<List<WordDTO>> _onData(LatLng? data, ref) async {
+
+  if(null == data) return [];
+
+  print(
+      'try to get words around ${data.latitude} ${data.longitude}');
+
+  final Dio dio = ref.read(dioProvider);
+
+  Response response = await dio.get(
+      '/word/around?latitude=${data.latitude}&longitude=${data.longitude}');
+
+  var jsonAsString = response.toString();
+  var json = jsonDecode(jsonAsString);
+
+  final WordsDTO wordsDTO = WordsDTO.fromJson(json);
+  if (wordsDTO.data == null) {
+    return Future.value([]);
+  }
+
+  print('test $wordsDTO');
+  return Future.value(wordsDTO.data!);
+}
+
+FutureOr<List<WordDTO>> _onError(Object error, StackTrace stackTrace) {
+  return [];
+}
+
+FutureOr<List<WordDTO>> _onLoading() {
+  return [];
+}
